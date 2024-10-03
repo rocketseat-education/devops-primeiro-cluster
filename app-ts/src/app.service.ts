9b29693f7c55b25e712b26576c697ed67fb0f6ba
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-10-03 18:11:18 -0300

    Explorando o objeto Secret

--- app-ts/src/app.service.ts
+++ app-ts/src/app.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(process.env.APP);
+    console.log(`ConfigMap: ${process.env.APP}`);
+    console.log(`Secret: ${process.env.API_KEY}`);
     return 'Hello World!';
   }
 
