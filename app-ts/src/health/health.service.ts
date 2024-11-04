5ba9441fd8d6bea05621c7a970b268bc321619c2
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-11-04 12:36:46 -0300

    Startup

--- app-ts/src/health/health.service.ts
+++ app-ts/src/health/health.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
   checkHealth(): string {
+    console.log('Chequei a saúde da aplicação!');
     return 'OK';
   }
   
   checkReady(): string {
+    console.log('Chequei a prontidão da aplicação!');
     return 'OK';
   }
 }
